be2ec5b07716d0ddb3597c97116d9b0cf4388496
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-10-12 17:43:08 +0600

    Update

---
+++ isp-ms/resources/views/layouts/master.blade.php
@@ -0,0 +1,33 @@
+@include('layouts.partials.header')
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+      <div class="layout-container">
+        <!-- Menu -->
+          @include('layouts.partials.sidebar')
+        <!-- / Menu -->
+
+        <!-- Layout container -->
+        <div class="layout-page">
+          <!-- Navbar -->
+
+          @include('layouts.partials.navbar')
+
+          <!-- / Navbar -->
+
+          <!-- Content wrapper -->
+          <div class="content-wrapper">
+            <!-- Content -->
+             <div class="container-xxl flex-grow-1 container-p-y">
+                <div className="card-body">
+                  <!-- Insert Your content here -->
+                  @yield('content')
+
+                  Start creating your amazing application!
+                  
+                </div>
+              </div>
+            <!-- / Content -->
+
+            <!-- Footer -->
+             @include('layouts.partials.footer')
+            <!-- / Footer -->
