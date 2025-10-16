980688b81ed70bb6176b166c1796a490185e9d8b
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-10-16 21:07:39 +0600

    Update

--- laravel-crud/resources/views/welcome.blade.php
+++ laravel-crud/resources/views/welcome.blade.php
@@ -8,56 +8,31 @@
 
 </head>
 <body>
-    <div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card shadow-lg">
-                <div class="card-header bg-primary text-white">
-                    <h4 class="mb-0">Create New Post</h4>
-                </div>
-                <div class="card-body">
-                    <form action="/posts" method="POST" enctype="multipart/form-data">
-                        @csrf 
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Post Name</label>
-                            <input type="text" 
-                                   class="form-control" 
-                                   id="name" 
-                                   name="name" 
-                                   placeholder="Enter the title of the post" 
-                                   required>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="description" class="form-label">Description</label>
-                            <textarea class="form-control" 
-                                      id="description" 
-                                      name="description" 
-                                      rows="5" 
-                                      placeholder="Write the full description of the post" 
-                                      required></textarea>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="image" class="form-label">Upload Image</label>
-                            <input type="file" 
-                                   class="form-control" 
-                                   id="image" 
-                                   name="image" 
-                                   accept="image/*" 
-                                   required>
-                            <div id="imageHelp" class="form-text">Please upload an image in JPG, PNG, or GIF format.</div>
-                        </div>
-                        
-                        <div class="d-grid gap-2 mt-4">
-                            <button type="submit" class="btn btn-success btn-lg">Submit Post</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        
+        <a class="navbar-brand" href="/">Laravel App</a>
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+            </ul>
+            
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="btn btn-outline-light" href="/create">Create New Post</a>
+                </li>
+            </ul>
         </div>
     </div>
-</div>
+</nav>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
